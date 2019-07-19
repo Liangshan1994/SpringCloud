9ba2c547de05bc7be945936dd9f6212d88408e3c
758831364@qq.com <758831364@qq.com>
2019-07-19 17:43:14 +0800

    添加路由服务

---
+++ SpringCloud-Zuul/src/main/java/com/pikaqiu/springcloud/zuul/SpringCloudZuulApplication.java
@@ -0,0 +1,18 @@
+package com.pikaqiu.springcloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringCloudApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class SpringCloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZuulApplication.class, args);
+    }
+
+}
