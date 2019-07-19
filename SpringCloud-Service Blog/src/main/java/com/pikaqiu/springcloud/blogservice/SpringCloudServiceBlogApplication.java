4a4cbe7a39ea397f0dbb2c015b1d69b9efd1f24a
758831364@qq.com <758831364@qq.com>
2019-07-19 15:56:55 +0800

    添加一个博客服务

---
+++ SpringCloud-Service Blog/src/main/java/com/pikaqiu/springcloud/blogservice/SpringCloudServiceBlogApplication.java
@@ -0,0 +1,13 @@
+package com.pikaqiu.springcloud.blogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServiceBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServiceBlogApplication.class, args);
+    }
+
+}
