63f2b680c61720d60a23b58ae70b76ace448c03b
758831364@qq.com <758831364@qq.com>
2019-07-19 15:56:45 +0800

    添加一个用户服务

---
+++ SpringCloud-Service User/src/main/java/com/pikaqiu/springcloud/userservice/SpringCloudServiceUserApplication.java
@@ -0,0 +1,13 @@
+package com.pikaqiu.springcloud.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServiceUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServiceUserApplication.class, args);
+    }
+
+}
